125ebd7c98f73cb1159ea4edba3bb3f383d32d66
Gregor Zweig <zwickzwackzwieback@gmx.de>
2022-06-08 15:20:05 +0200

    chore: make pretty

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,18 @@
-const { shareAll, withModuleFederationPlugin } = require('@angular-architects/module-federation/webpack');
+const {
+  shareAll,
+  withModuleFederationPlugin,
+} = require("@angular-architects/module-federation/webpack");
 
 module.exports = withModuleFederationPlugin({
-  name: 'angular-service',
+  name: "angular-service",
   exposes: {
-    './ServiceModule': './src/app/service/service.module.ts',
+    "./ServiceModule": "./src/app/service/service.module.ts",
   },
   shared: {
-    ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
+    ...shareAll({
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: "auto",
+    }),
   },
 });
